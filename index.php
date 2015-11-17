bb53a54a982343bd779d38dbb920f72f47bc22ec
munHunger <marcusmunger@hotmail.com>
2015-11-17 21:17:27 +0100

    Scene base commit

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <html>
-	<title>
-		Stormy Neutron
-	</title>
+	<head>
+		<title>
+			Stormy Neutron
+		</title>
+        <script src="http://www.babylonjs.com/hand.minified-1.2.js"></script>
+        <script src="http://www.babylonjs.com/cannon.js"></script>
+        <script src="http://www.babylonjs.com/oimo.js"></script>
+        <script src="http://www.babylonjs.com/babylon.js"></script>
+	</head>
 	<body>
-		
+		<canvas id="renderCanvas"></canvas>
+		<script type="text/javascript" src="clientSide/main.js"></script>
 	</body>
 </html>
